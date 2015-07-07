223988098d6a89ab3f953181afb3d02c742ecad6
mingezhao <mingezhao@gmail.com>
2015-07-08 00:48:52 +0800

    Temp submit.

--- Technologies.AspNet5/src/Technologies.AspNet5/Controllers/IconsController.cs
+++ Technologies.AspNet5/src/Technologies.AspNet5/Controllers/IconsController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net.Http;
 
 namespace Technologies.AspNet5.Controllers
 {
     public class IconsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var fontAwesomeClasses = new List<string>();
+
+            var content = string.Empty;
+            using (var client = new HttpClient()) {
+                content = await client.GetStringAsync(new Uri(new Uri("http://"+this.Request.Host.ToString()), Url.Content("~/lib/font-awesome/css/font-awesome.css")));
+            }
+
+            MatchCollection match = Regex.Matches(content, @"\.(fa[a-z|A-Z|-]*):before", RegexOptions.IgnoreCase);
+
             return View();
         }
     }

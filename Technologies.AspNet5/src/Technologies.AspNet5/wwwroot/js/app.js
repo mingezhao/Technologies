9a87540157d2b54c4332ed3788245877912c756a
mingezhao <mingezhao@gmail.com>
2015-07-13 09:04:04 +0800

    Temp commit.

--- Technologies.AspNet5/src/Technologies.AspNet5/wwwroot/js/app.js
+++ Technologies.AspNet5/src/Technologies.AspNet5/wwwroot/js/app.js
@@ -20,4 +20,49 @@
         ];
     });
 
+    app.directive('headerBanner', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<input maxlength="10"></input>',
+            link: function (scope, element, attrs, ctrl) {
+                ctrl.$validators.fax = function (modelValue, viewValue) {
+                    if (ctrl.$isEmpty(modelValue)) {
+                        return true;
+                    }
+
+                    if (FAX_REGEXP.test(viewValue)) {
+                        return true;
+                    }
+
+                    return false;
+                };
+            }
+        };
+    });
+
+
+
+    app.directive('headerContent', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'template/header/header-content.html',
+            scope: {
+                title: '@',
+                desc: '@',
+            }
+        };
+    });
+
+    app.run(function ($templateCache) {
+        $templateCache.put('template/header/header-content.html',
+          '<header class="bs-header text-center">\n' +
+          ' <div class="container">\n' +
+          '     <h1>{{title}}</h1>\n' +
+          '     <p>{{desc}}</p>\n' +
+          ' </div>' +
+          '</header>');
+    });
+
 })(window, angular)

e32919a45dbae82c39edba59cde8ff9899a56c4e
mingezhao <mingezhao@gmail.com>
2015-07-13 12:40:00 +0800

    Temp commit.

---
+++ Technologies.AspNet5/src/Technologies.AspNet5/wwwroot/js/script-reference.js
@@ -0,0 +1,12 @@
+﻿require.config({
+    paths: {
+        //jquery: '../lib/jquery/jquery',
+        angular: '../lib/angular/angular',
+        angularBootstrap: '../lib/angular-bootstrap/ui-bootstrap-tpls',
+        app: 'app'
+    }
+});
+
+require(['angular', 'angularBootstrap', 'app'], function ($) {
+
+});

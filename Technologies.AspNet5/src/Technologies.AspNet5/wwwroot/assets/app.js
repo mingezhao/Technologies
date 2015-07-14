780bf6260bdd7dd7fe7f329db36a8027cd2aa108
mingezhao <mingezhao@gmail.com>
2015-07-14 16:29:28 +0800

    Temp commit.

--- Technologies.AspNet5/src/Technologies.AspNet5/wwwroot/assets/app.js
+++ Technologies.AspNet5/src/Technologies.AspNet5/wwwroot/assets/app.js
@@ -1,92 +1,165 @@
 ﻿(function (window, angular) {
     'use strict';
 
-    window.app = angular.module('technologiesApp', ['ui.bootstrap']);
+    /**
+     * Config Module
+     */
+    angular.module('app.config', []).
+        factory('modules', function () {
+            return [
+                {
+                    order: 1,
+                    key: 'Icons',
+                    name: 'Icons',
+                    src: '/icons/index.html',
+                    desc: 'Glyphicons & Font Awesome',
+                },
+                {
+                    order: 2,
+                    key: 'jQuery',
+                    name: 'jQuery',
+                    src: '../jquery',
+                    desc: '2222222'
+                }
+            ];
+        });
 
-    app.factory('modules', function () {
-        return [
-            {
-                order: 1,
-                key: 'Icons',
-                name: 'Icons',
-                src: '/icons/index.html',
-                desc: '11111'
-            },
-            {
-                order: 2,
-                key: 'jQuery',
-                name: 'jQuery',
-                src: '../jquery',
-                desc: '2222222'
-            }
-        ];
-    });
+    /**
+     * Directives Module
+     */
+    angular.module('app.directives', ['app.config', 'app.directives.header', 'app.directives.footer']);
 
-    app.directive('headerBanner', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'template/header/header-banner.html',
-            controller: function ($scope, modules) {
-                $scope.modules = modules;
-            }
-        };
-    });
+    /**
+     * Header Directive
+     */
+    angular.module('app.directives.header', []).
+        controller('HeaderCtrl', function ($scope, modules) {
+            this.getModuldes = function () {
+                return modules;
+            };
 
-    app.directive('headerContent', function (modules) {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'template/header/header-content.html',
-            scope: {
-                title: '=',
-                desc: '=',
-                moduleKey: '='
-            },
-            controller: function ($scope, modules) {
-                if (angular.isUndefined($scope.title) && angular.isUndefined($scope.desc) && !angular.isUndefined($scope.moduleKey)) {
-                    $scope.title = 'test';
-                    $scope.desc = 'desc';
+            this.getModule = function (key) {
+                for (var i = 0; i < modules.length; i++) {
+                    if (angular.equals(angular.lowercase(modules[i].key), angular.lowercase(key))) {
+                        return modules[i];
+                    }
                 }
             }
-        };
-    });
+        }).
+        directive('headerBody', function () {
+            return {
+                restrict: 'AE',
+                replace: true,
+                transclude: true,
+                templateUrl: 'template/header-body.html',
+                scope: {
+                    moduleKey: '@'
+                },
+                controller: 'HeaderCtrl',
+                link: function (scope, element, attrs, headerCtrl) {
+                    if (angular.isUndefined(scope.moduleKey)) {
+                        scope.title = 'Technologies';
+                    }
+                    else {
+                        var module = headerCtrl.getModule(scope.moduleKey);
+                        scope.title = module.name;
+                        scope.desc = module.desc;
+                    }
+                }
+            };
+        }).
+        directive('headerMenus', function () {
+            return {
+                restrict: 'AE',
+                replace: true,
+                require: '^headerBody',
+                template: function (element, attrs) {
+                    var menusHtml =
+                        '<nav>\n' +
+                        '   <a class="navbar-brand" href="">Technologies</a>\n' +
+                        '   <ul class="nav navbar-nav">\n' +
+                        '       <li class="dropdown" dropdown>\n' +
+                        '           <span role="button" class="dropdown-toggle" dropdown-toggle>\n' +
+                        '               Directives <b class="caret"></b>\n' +
+                        '           </span>\n' +
+                        '           <ul class="dropdown-menu">\n' +
+                        '               <li ng-repeat="module in modules">\n' +
+                        '                   <a ng-href="{{module.src}}">{{module.name}}</a>\n' +
+                        '               </li>\n' +
+                        '           </ul>\n' +
+                        '       </li>\n'
 
-    app.run(function ($templateCache) {
-        $templateCache.put('template/header/header-banner.html',
-          '<header class="navbar navbar-default navbar-fixed-top">\n' +
-          ' <div class="navbar-inner">\n' +
-          '     <div class="container">\n' +
-          '         <nav>\n' +
-          '             <a class="navbar-brand" href="@Url.Content(" ~ />")">@AppSettings.Options.SiteTitle</a>\n' +
-          '             <ul class="nav navbar-nav">\n' +
-          '                 <li class="dropdown" dropdown>\n' +
-          '                     <span role="button" class="dropdown-toggle" dropdown-toggle>\n' +
-          '                         Directives <b class="caret"></b>\n' +
-          '                     </span>\n' +
-          '                     <ul class="dropdown-menu">\n' +
-          '                         <li ng-repeat="module in modules">\n' +
-          '                             <a ng-href="{{module.src}}">{{module.name}}</a>\n' +
-          '                         </li>\n' +
-          '                     </ul>\n' +
-          '                 </li>\n' +
-          '                 <li><a href="https://github.com/mingezhao/Technologies"><i class="fa fa-github"></i>&nbsp;Git Hub</a></li>\n' +
-          '             </ul>\n' +
-          '         </nav>\n' +
-          '     </div>\n' +
-          ' </div>\n' +
-          '</header>\n'
-          );
-    });
+                    angular.forEach(element.children(), function (item, index) {
+                        menusHtml += item.outerHTML;
+                    });
 
-    app.run(function ($templateCache) {
-        $templateCache.put('template/header/header-content.html',
-          '<header class="bs-header text-center">\n' +
-          ' <div class="container">\n' +
-          '     <h1>{{title}}</h1>\n' +
-          '     <p>{{desc}}</p>\n' +
-          ' </div>' +
-          '</header>');
-    });
+                    menusHtml +=
+                        '       <li><a href="https://github.com/mingezhao/Technologies"><i class="fa fa-github"></i>&nbsp;Git Hub</a></li>\n' +
+                        '   </ul>\n' +
+                        '</nav>\n';
+
+                    return menusHtml;
+                },
+                link: function (scope, element, attrs, headerCtrl) {
+                    scope.modules = headerCtrl.getModuldes();
+                }
+            };
+        }).
+        run(function ($templateCache) {
+            $templateCache.put('template/header-body.html',
+                '<div role="header">\n' +
+                '   <header class="navbar navbar-default navbar-fixed-top">\n' +
+                '       <div class="navbar-inner">\n' +
+                '           <div class="container" ng-transclude>\n' +
+                '               <nav>\n' +
+                '                   <a class="navbar-brand" href=""></a>\n' +
+                '                   <ul class="nav navbar-nav">\n' +
+                '                       <li class="dropdown" dropdown>\n' +
+                '                           <span role="button" class="dropdown-toggle" dropdown-toggle>\n' +
+                '                               Directives <b class="caret"></b>\n' +
+                '                           </span>\n' +
+                '                           <ul class="dropdown-menu">\n' +
+                '                               <li ng-repeat="module in modules">\n' +
+                '                                   <a ng-href="{{module.src}}">{{module.name}}</a>\n' +
+                '                               </li>\n' +
+                '                           </ul>\n' +
+                '                       </li>\n' +
+                '                       <li><a href="https://github.com/mingezhao/Technologies"><i class="fa fa-github"></i>&nbsp;Git Hub</a></li>\n' +
+                '                   </ul>\n' +
+                '               </nav>\n' +
+                '           </div>\n' +
+                '       </div>\n' +
+                '   </header>\n' +
+                '   <header class="bs-header text-center">\n' +
+                '       <div class="container">\n' +
+                '           <h1>{{title}}</h1>\n' +
+                '           <p>{{desc}}</p>\n' +
+                '       </div>\n' +
+                '   </header>\n' +
+                '<div>\n'
+              );
+        });
+
+    /**
+     * Footer Directive
+     */
+    angular.module('app.directives.footer', []).
+        directive('footerBody', function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'template/footer-body.html'
+            };
+        }).
+        run(function ($templateCache) {
+            $templateCache.put('template/footer-body.html',
+                '<footer class="bs-header text-center">\n' +
+                '</footer>');
+        });
+
+    /**
+     * Init app for each page
+     */
+    window.app = angular.module('app', ['ui.bootstrap', 'app.config', 'app.directives']);
 
 })(window, angular)
